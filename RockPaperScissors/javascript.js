941c5edd26501f0146377e382c8a08f6401a124b
chudler624odin <134406094+chudler624odin@users.noreply.github.com>
2023-06-20 22:08:03 -0400

    Complete Game in Console

--- RockPaperScissors/javascript.js
+++ RockPaperScissors/javascript.js
@@ -1,69 +1,81 @@
-
-//function game(){
-
-//}
-//user selection
-var userSelection = prompt("Select: Rock, Paper, Scissors").toLowerCase().toString()
-
-
-
-//computer selection
-let choices =["rock", "paper", "scissors"]
-var randomIndex = Math.floor(Math.random() * choices.length); 
-var computerSelection = choices[randomIndex];
-
 //initialize counter
-let userWins=0;
-let computerWins=0;
+let userWins = 0;
+let computerWins = 0;
+let gamesPlayed = 0;
 
+//user's selection
+function getUserSelection() {
+    var userSelection = prompt("Select: Rock, Paper, Scissors").toLowerCase();
 
+    return userSelection;
+}
+
+//computer's selection
+function getComputerSelection() {
+    let choices = ["rock", "paper", "scissors"]
+    var randomIndex = Math.floor(Math.random() * choices.length);
+    var computerSelection = choices[randomIndex];
+
+    return computerSelection;
+
+}
 
 //game rules
+function gamePlay(userSelection, computerSelection){
 
-if(userSelection == "rock" && computerSelection == "scissors"){
+    if(userSelection == "rock" && computerSelection == "scissors"){
+    
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`); userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else if(userSelection == "rock" && computerSelection == "paper"){
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else if(userSelection == "paper" && computerSelection == "rock"){
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else if(userSelection == "paper" && computerSelection == "scissors"){
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else if(userSelection == "scissors" && computerSelection == "paper"){
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else if(userSelection == "scissors" && computerSelection == "rock"){
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+    }
+    else{
+        console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);console.log("Tie! No winner here")
+    }
 
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`); userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+}       
+
+function playRound(){
+    userSelection = getUserSelection();
+    computerSelection = getComputerSelection();
+    gamePlay(userSelection, computerSelection);
 }
-else if(userSelection == "rock" && computerSelection == "paper"){
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
+
+function playGame(){
+
+    //play x amount of rounts
+    for(gamesPlayed=0; gamesPlayed < 5; gamesPlayed++){
+        console.log("Round " + (gamesPlayed + 1) + " Start");
+        playRound();
+    }
+
+    //display end game msg
+    let endGameMsg = '';
+    if(userWins > computerWins)
+    {
+        endGameMsg = `User Wins!`;
+    } else {
+        endGameMsg = 'Computer Wins!';
+    }
+
+    console.log(endGameMsg);
 }
-else if(userSelection == "paper" && computerSelection == "rock"){
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
-}
-else if(userSelection == "paper" && computerSelection == "scissors"){
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
-}
-else if(userSelection == "scissors" && computerSelection == "paper"){
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);userWins++; console.log("User is Victorious!"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
-}
-else if(userSelection == "scissors" && computerSelection == "rock"){
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);computerWins++; console.log("computer wins this one"); console.log(`User Wins: ${userWins} Computer Wins: ${computerWins}`)
-}
-else{
-    console.log(`You chose ${userSelection}, the Computer chose ${computerSelection}`);console.log("Tie! No winner here")
 
-}
+playGame();
+    
+    
+    
 
-
-
-
-//console.log(userSelection)
-//console.log(computerSelection)
-
-
-
-
-
-
-function rockPaperScissors(){
-
-
-
-    let rock
-    let paper
-    let scissors
-
-    rock > scissors;
-    scissors > paper;
-    paper > rock;
-}
